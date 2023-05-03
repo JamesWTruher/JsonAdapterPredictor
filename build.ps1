bea3adb43d07a8fbaeef0907c86a77c693b4a4db
James Truher <jimtru@microsoft.com>
2023-05-03 16:43:41 -0700

    Complete initial json adapter.
    This adapter finds both <name>-json,
    and uses of `jc` that are supported.

--- build.ps1
+++ build.ps1
@@ -1,12 +1,29 @@
 param (
-    [switch]$Build,
+    [switch]$Publish,
+    [switch]$Release,
     [switch]$Test
 )
 
-if ($Build) {
+$config = $Release.ToBool() ? "Release" : "Debug"
+if ( ! $Publish ) {
+    wait-debugger
     try {
-        Push-Location src
-        dotnet publish
+        Push-Location "$PSScriptRoot/src"
+        dotnet build --configuration $config
+    }
+    finally {
+        pop-location
+    }
+}
+else {
+    try {
+        Push-Location "$PSScriptRoot/src"
+        if ( $Release ) {
+            dotnet publish --configuration Release
+        }
+        else {
+            dotnet publish --configuration Debug
+        }
     }
     finally {
         Pop-Location
